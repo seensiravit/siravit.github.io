74c27d72e9d53b56bd3a75c43348f9631a345b80
seensiravit <s.keeratipranon@gmail.com>
2023-11-06 21:27:43 +0700

    transform file from last folder

---
+++ data/dict.js
@@ -0,0 +1,8 @@
+export let wordDict = [
+    { word: 'apple' , definition: 'red round fruit' } , 
+    {word: 'banana' , definition: 'yellow fruit'} , 
+    {word: 'carrot' , definition: 'rabbit favorite'} , 
+    {word: 'orange' , definition: 'orange fruit'}
+];
+
+// for(let i=0 ; wordDict[i] ; i++) console.log(wordDict[i].word);
---
+++ script/wordGuessing.js
@@ -0,0 +1,5 @@
+import { wordDict } from "../data/dict.js";
+
+wordDict.forEach((value , index) => {
+    console.log(index , value);
+});
