0f14f8bbff308477a43c0e6cb76ef3d6d43a7476
seensiravit <s.keeratipranon@gmail.com>
2023-11-06 22:00:25 +0700

    add random word button and show ans section

--- script/wordGuessing.js
+++ script/wordGuessing.js
@@ -4,10 +4,22 @@ wordDict.forEach((value , index) => {
     console.log(index , value);
 });
 
+let word_idex = 0;
+
 function get_word() {
     const random_number = Math.random();
     const number = Math.floor(random_number * 100);
     const index = number % wordDict.length;
 
-    return index;
+    word_idex = index;
 }
+
+function show_answer() {
+    document.querySelector('.js-answer').innerHTML = `${wordDict[word_idex].word}`;
+}
+
+document.querySelector('.js-random-word').addEventListener('click' , () => {
+    get_word();
+    console.log(word_idex);
+    show_answer();
+});
