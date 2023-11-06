d360cc2f99a9ea21e4692283b6979242e6949b3b
seensiravit <s.keeratipranon@gmail.com>
2023-11-06 21:46:56 +0700

    add submit button and create get_word function

--- script/wordGuessing.js
+++ script/wordGuessing.js
@@ -3,3 +3,11 @@ import { wordDict } from "../data/dict.js";
 wordDict.forEach((value , index) => {
     console.log(index , value);
 });
+
+function get_word() {
+    const random_number = Math.random();
+    const number = Math.floor(random_number * 100);
+    const index = number % wordDict.length;
+
+    return index;
+}
